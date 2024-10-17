88ae55428935d35ce1f74de1cde5302b81405af5
darshh399 <sudarshanshinde399@gmail.com>
2024-10-17 17:18:09 +0530

    logic implemented for if input is 0 or single number.

---
+++ Calculator.js
@@ -0,0 +1,6 @@
+function add(numbers) {
+    if (!numbers) return 0; // Will return 0 if numbers not contains or empty
+    if (numbers.length === 1) return Number(numbers); // Will return same number if only one number available
+
+    
+}
