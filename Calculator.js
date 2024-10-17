6b0fb425fb2e3db1d85456261a5218c0b21757b0
darshh399 <sudarshanshinde399@gmail.com>
2024-10-17 17:21:49 +0530

    logic implemented for delimeter string .

--- Calculator.js
+++ Calculator.js
@@ -2,5 +2,11 @@ function add(numbers) {
     if (!numbers) return 0; // Will return 0 if numbers not contains or empty
     if (numbers.length === 1) return Number(numbers); // Will return same number if only one number available
 
-    
+        
+        let delimiter = ','; // Default delimiter
+        if (numbers.startsWith('//')) {
+            const parts = numbers.split('\n');
+            delimiter = parts[0].substring(2); // find and remove delimeters length
+            numbers = parts[1]; // final number
+        }
 }
